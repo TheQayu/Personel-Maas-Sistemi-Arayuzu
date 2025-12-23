c390ebc1c97da57fe78279b7e23e1a0dc4a80e24
Mehmet Oğuzhan Tanrıverdi <mehmetoguzhan2504@gmail.com>
2025-12-24 01:03:58 +0300

    Add project files.

---
+++ denemelikimid/PersonelVerisi.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic; // Dictionary için gerekli
+using System.Linq;              // Count fonksiyonu için gerekli
+
+namespace denemelikimid  // <-- Buranın proje adınızla aynı olduğundan emin olun
+{
+    public class PersonelVerisi
+    {
+        // Temel Bilgiler (Puantaj.csv'den)
+        public string TCNo { get; set; }
+        public string AdSoyad { get; set; }
+        public string IBAN { get; set; } // Banka Listesi için
+
+        // Puantaj Bilgisi (1-31 Gün)
+        // Her gün için durumu tutar: "X", "R" (Rapor), "İ" (İzin) veya boş
+        public Dictionary<int, string> GunlukPuantaj { get; set; } = new Dictionary<int, string>();
+
+        // Hesaplama Parametreleri (BORDRO.csv için)
+        public decimal GunlukBrutUcret { get; set; } // Örn: 666.75 TL (2024 Asgari)
+
+        // Hesaplanan Veriler (Otomatik dolacak)
+        // "X" veya "Tam" olan günleri sayar
+        public int ToplamCalismaGunu => GunlukPuantaj.Count(x => x.Value == "X" || x.Value == "Tam");
+
+        public decimal ToplamBrut => ToplamCalismaGunu * GunlukBrutUcret;
+
+        public decimal NetEleGecen => MaasHesapla(ToplamBrut);
+
+        // Basit bir Net Maaş Hesaplama Fonksiyonu
+        private decimal MaasHesapla(decimal brut)
+        {
+            // ÖRNEK: Sadece Damga Vergisi kesintisi varsa (0.00759)
+            // İŞKUR kurallarına göre burayı sonra güncelleriz
+            decimal damgaVergisi = brut * 0.00759m;
+            return brut - damgaVergisi;
+        }
+    }
+}
---
+++ denemelikimid/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("denemelikimid")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("denemelikimid")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fad8eab7-bb22-4163-ac68-b056a705266d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
